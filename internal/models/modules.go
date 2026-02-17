eea3651975186bed6485d7aa4edc041a49d467b8
Kirill <masteryoda01010101@gmail.com>
2026-02-17 20:46:37 +0300

    update Task model for database sql questions

--- internal/models/modules.go
+++ internal/models/modules.go
@@ -4,12 +4,12 @@ import "time"
 
 // todo немного доработать
 type Task struct {
-	ID          int       `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	Completed   bool      `json:"completed"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID          int       `json:"id" db:"id"`
+	Title       string    `json:"title" db:"title"`
+	Description string    `json:"description" db:"description"`
+	Completed   bool      `json:"completed" db:"completed"`
+	CreatedAt   time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at" db:"updated_at"`
 }
 
 type CreateTaskInput struct {

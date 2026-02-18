68656f1014da058bce6654f3c808d4ccd79a6347
Kirill <masteryoda01010101@gmail.com>
2026-02-18 16:39:21 +0300

    three methods complited

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,7 +3,10 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"strings"
 	"task-manager-go/internal/database"
+	"task-manager-go/internal/models"
 )
 
 type TaskHandlers struct {
@@ -20,20 +23,62 @@ func respondWithJSON(w http.ResponseWriter, statusCode int, payload interface{})
 	json.NewEncoder(w).Encode(payload) // создаем новый Encoder, который пишет в ResponseWriter, Encode преобразует поле в jsonи отправляет ответ
 }
 
-func respondWithError() {
-	// todo
+func respondWithError(w http.ResponseWriter, statusCode int, message string) {
+	respondWithJSON(w, statusCode, map[string]string{"error": message})
 }
 
+// endpoint GET /tasks
 func (t *TaskHandlers) GetAllTask(w http.ResponseWriter, r *http.Request) {
-	// todo
+	task, err := t.store.GetAll()
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Ошибка получения задач")
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, task)
 }
 
+// endpoint GET /tasks/{id} <- этот айди надо извлечь из url пути
 func (t *TaskHandlers) GetTask(w http.ResponseWriter, r *http.Request) {
-	// todo
+	// тут мы разбиваем путь по айди
+	// разбиваем путь r.URL.Path по "/" и берем последний элемент id -> GET /tasks/{id}
+	pathParts := strings.Split(strings.TrimPrefix(r.URL.Path, "/tasks/"), "/")
+	idStr := pathParts[0]
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Получен невалидный id")
+	}
+
+	task, err := t.store.GetByID(id)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+	}
+
+	respondWithJSON(w, http.StatusOK, task)
 }
 
+// endpoint POST /task -> новая задача
 func (t *TaskHandlers) CreateTask(w http.ResponseWriter, r *http.Request) {
-	// todo
+	var input models.CreateTaskInput
+
+	err := json.NewDecoder(r.Body).Decode(&input) // декодируем тело запроса и вставляем в структуру input
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Некорректные данные запроса")
+	}
+
+	// todo validation to title
+	if strings.TrimSpace(input.Title) == "" {
+		respondWithError(w, http.StatusBadRequest, "Заголовок к задаче обязателен")
+	}
+
+	task, err := t.store.Create(input)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Ошибка создания задачи")
+	}
+
+	respondWithJSON(w, http.StatusOK, task)
 }
 
 func (t *TaskHandlers) UpdateTask(w http.ResponseWriter, r *http.Request) {

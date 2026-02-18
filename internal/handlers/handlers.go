5d48ae5d0e8a777dd43bbce8d43d5443cc85f8f6
Kirill <masteryoda01010101@gmail.com>
2026-02-18 15:48:28 +0300

    update func respondWithJson

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,13 +1,23 @@
 package handlers
 
-import "task-manager-go/internal/database"
+import (
+	"encoding/json"
+	"net/http"
+	"task-manager-go/internal/database"
+)
 
 type TaskHandlers struct {
 	store database.TaskStore
 }
 
-func respondWithJSON() {
-	// todo вспомогательная функция для отправки хэндлера
+func respondWithJSON(w http.ResponseWriter, statusCode int, payload interface{}) {
+	// поле payload - данные, которые нужно отправлять в формате json
+	// w http.ResponseWriter - используется для отправки ответа
+	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(statusCode)
+
+	json.NewEncoder(w).Encode(payload) // создаем новый Encoder, который пишет в ResponseWriter, Encode преобразует поле в jsonи отправляет ответ
 }
 
 func respondWithError() {

c681af74f1719b77bfa19bb3725f5f187188b8e7
Kirill <masteryoda01010101@gmail.com>
2026-02-18 12:54:56 +0300

    add new methods from tasks update and delete

--- internal/database/tasks.go
+++ internal/database/tasks.go
@@ -81,3 +81,72 @@ func (t *TaskStore) Create(input models.CreateTaskInput) (*models.Task, error) {
 
 	return &task, nil
 }
+
+func (t *TaskStore) Update(id int, input models.UpdateTaskInput) (*models.Task, error) {
+	task, err := t.GetByID(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if input.Title != nil {
+		task.Title = *input.Title
+	}
+
+	if input.Description != nil {
+		task.Description = *input.Description
+	}
+
+	if input.Completed != nil {
+		task.Completed = *input.Completed
+	}
+
+	var updatedTask models.Task
+
+	task.UpdatedAt = time.Now()
+
+	query := `UPDATE tasks SET title = $1, description = $2, completed = $3, updated_at = $4
+	WHERE id = $5
+	RETURNING id, title, description, created_at, updated_at`
+
+	err = t.db.QueryRowx(
+		query,
+		updatedTask.Title,
+		updatedTask.Description,
+		updatedTask.Completed,
+		task.UpdatedAt,
+		id,
+	).StructScan(&updatedTask)
+
+	if err != nil {
+		return nil, fmt.Errorf("ошибка обновления задачи", err)
+	}
+
+	return &updatedTask, nil
+}
+
+func (t *TaskStore) Delete(id int) error {
+
+	query := `
+	DELETE FROM tasks
+	WHERE id = $1
+	`
+
+	result, err := t.db.Exec(query, id) // функция Exeс выполняет запрос, не возвращая ни одной строки
+
+	if err != nil {
+		return fmt.Errorf("ошибка проверки результата удаления задачи", err)
+	}
+
+	// RowsAffected возвращает кол-во затронутых (удаленных) строк
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("ошибка проверки удаления", err)
+	}
+
+	if rowsAffected == 0 {
+		fmt.Errorf("задача с id %d не найдена", id)
+	}
+
+	return nil
+}

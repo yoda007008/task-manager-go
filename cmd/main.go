01ba475f4def333010e81b9f3f6bafc23a842377
Kirill <masteryoda01010101@gmail.com>
2026-02-21 15:52:45 +0300

    working task-manager-service in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,39 +2,61 @@ package main
 
 import (
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"task-manager-go/internal/database"
 	"task-manager-go/internal/handlers"
 )
 
 func main() {
-	//// todo run service
-	//databaseURL := os.Getenv("DATABASE_URL") // todo make directory config and parse config
-	//if databaseURL == "" {
-	//	databaseURL = "postgresql://taskuser:taskpass@localhost:5432/taskdb?sslmode=disable"
-	//}
-	//
-	//serverPort := os.Getenv("SERVER_PORT")
-	//if serverPort == "" {
-	//	serverPort = "8080"
-	//}
-	//
-	//slog.Info("Запуск приложения TaskManagerAPI...")
-	//
-	//db, err := database.Connect(databaseURL)
-	//if err != nil {
-	//	slog.Error("Ошибка подключения к базе", err)
-	//}
-	//
-	//defer db.Close()
-	//
-	//slog.Info("Успешное подключение к базе...")
-	//
-	//taskStore := database.NewTaskStore(db)
-	//
-	//handler := handlers.NewTaskHandler(taskStore)
-	//
-	//mux := http.NewServeMux()
+	// todo run service with graceful shutdown
+	databaseURL := os.Getenv("DATABASE_URL") // todo make directory config and parse config
+	if databaseURL == "" {
+		databaseURL = "postgresql://taskuser:taskpass@localhost:5440/taskdb?sslmode=disable"
+	}
 
+	serverPort := os.Getenv("SERVER_PORT")
+	if serverPort == "" {
+		serverPort = "8080"
+	}
+
+	slog.Info("Запуск приложения TaskManagerAPI...")
+
+	db, err := database.Connect(databaseURL)
+	if err != nil {
+		slog.Error("Ошибка подключения к базе", err)
+	}
+
+	defer db.Close()
+
+	slog.Info("Успешное подключение к базе...")
+
+	taskStore := database.NewTaskStore(db)
+
+	handler := handlers.NewTaskHandler(taskStore)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/tasks", methodHandler(handler.GetAllTask, "GET"))
+	mux.HandleFunc("/tasks/create", methodHandler(handler.CreateTask, "POST"))
+	mux.HandleFunc("/tasks/", taskIDHandler(handler))
+
+	loggerMux := loggingMiddleware(mux)
+	corsHandler := corsMiddleware(loggerMux) // если фронтенд приложение работает на другом домене, то добавляем CORS
+
+	log.Printf("Сервер запущен на порту %s", serverPort)
+	slog.Info("Доступные endpoints: ")
+	slog.Info("  GET 	 /tasks - получить все задачи")
+	slog.Info("  GET    /tasks/{id}  - Получить задачу по ID")
+	slog.Info("  POST   /tasks/create - Создать новую задачу")
+	slog.Info("  PUT    /tasks/{id}  - Обновить задачу")
+	slog.Info("  DELETE /tasks/{id}  - Удалить задачу")
+
+	err = http.ListenAndServe(":"+serverPort, corsHandler)
+	if err != nil {
+		slog.Error("Ошибка запуска сервера", err)
+	}
 }
 
 // функция helper для проверки http метода

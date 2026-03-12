40243bfe52fe7761f16dae1b6d0990e2082ab0e2
Kirill <masteryoda01010101@gmail.com>
2026-03-12 21:11:14 +0300

    new file for testing logic

---
+++ internal/repository/user_repo_test.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"testing"
+)
+
+func TestUserHandlers_GetAll(t *testing.T) {
+	// todo testing
+}
+
+func TestUserHandlers_GetByID(t *testing.T) {
+	// todo testing
+}
+
+func TestUserHandlers_Create(t *testing.T) {
+	// todo testing
+}
+
+func TestUserHandlers_Update(t *testing.T) {
+	// todo testing
+}
+
+func TestUserHandlers_Delete(t *testing.T) {
+	// todo testing
+}

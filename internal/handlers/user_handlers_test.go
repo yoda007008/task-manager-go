5faeba0c158d53485c025a032eb0b2e7d6e4865f
Kirill <masteryoda01010101@gmail.com>
2026-03-22 11:01:42 +0300

    testing method GetAllTask

--- internal/handlers/user_handlers_test.go
+++ internal/handlers/user_handlers_test.go
@@ -1,9 +1,60 @@
 package handlers
 
-import "testing"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"task-manager-go/internal/models"
+	"task-manager-go/mocks"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 func TestTaskHandlers_GetAllTask(t *testing.T) {
 	// todo testing
+	mockRepo := mocks.NewTaskService(t)
+
+	handler := NewTaskHandler(mockRepo)
+
+	t.Run("success returning tasks", func(t *testing.T) {
+		expectedTasks := []models.Task{
+			{ID: 1, Title: "Task 1", Completed: false},
+			{ID: 2, Title: "Task 2", Completed: true},
+		}
+
+		mockRepo.On("GetAll").Return(expectedTasks, nil).Once()
+
+		req := httptest.NewRequest(http.MethodGet, "/tasks", nil)
+		w := httptest.NewRecorder()
+
+		handler.GetAllTask(w, req)
+
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.Equal(t, "application/json", w.Header().Get("Content-Type"))
+
+		var response []models.Task
+		err := json.Unmarshal(w.Body.Bytes(), &response)
+
+		require.NoError(t, err)
+		assert.Equal(t, expectedTasks, response)
+
+		mockRepo.AssertExpectations(t)
+	})
+
+	t.Run("returning error", func(t *testing.T) {
+		mockRepo.On("GetAll").Return(nil, errors.New("db error")).Once()
+
+		req := httptest.NewRequest(http.MethodGet, "/tasks", nil)
+		w := httptest.NewRecorder()
+
+		handler.GetAllTask(w, req)
+
+		assert.Equal(t, http.StatusInternalServerError, w.Code)
+		mockRepo.AssertExpectations(t)
+	})
 }
 
 func TestUserHandlers_GetByID(t *testing.T) {

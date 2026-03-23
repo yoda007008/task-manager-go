cd46b1c3ac9d7eceef5cb31e482f422139731aca
Kirill <masteryoda01010101@gmail.com>
2026-03-23 21:16:19 +0300

    add new test for method CreateTask and Create (coverage 43.5%)

--- internal/handlers/user_handlers_test.go
+++ internal/handlers/user_handlers_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"net/http"
@@ -98,9 +99,61 @@ func TestUserHandlers_GetByID(t *testing.T) {
 	})
 }
 
-//func TestUserHandlers_Create(t *testing.T) {
-//	// todo testing
-//}
+func TestUserHandlers_Create(t *testing.T) {
+	mockRepo := mocks.NewTaskService(t)
+
+	handler := NewTaskHandler(mockRepo)
+
+	t.Run("success create task, status 200", func(t *testing.T) {
+		input := models.CreateTaskInput{
+			Title:       "First task",
+			Description: "Run this program success",
+			Completed:   true,
+		}
+
+		expectedTask := &models.Task{
+			ID:          1,
+			Title:       input.Title,
+			Description: input.Description,
+			Completed:   input.Completed,
+		}
+
+		mockRepo.On("Create", input).Return(expectedTask, nil).Once()
+
+		body, err := json.Marshal(input)
+		require.NoError(t, err)
+
+		resp := httptest.NewRequest(http.MethodPost, "/tasks/", bytes.NewReader(body))
+		w := httptest.NewRecorder()
+
+		handler.CreateTask(w, resp)
+
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.Equal(t, "application/json", w.Header().Get("Content-Type"))
+
+		var response models.Task
+		err = json.Unmarshal(w.Body.Bytes(), &response)
+
+		require.NoError(t, err)
+		assert.Equal(t, expectedTask.ID, response.ID)
+		assert.Equal(t, expectedTask.Title, response.Title)
+		assert.Equal(t, expectedTask.Description, expectedTask.Description)
+		assert.Equal(t, expectedTask.Completed, expectedTask.Completed)
+
+		mockRepo.AssertExpectations(t)
+	})
+
+	t.Run("not success response, status 400", func(t *testing.T) {
+		resp := httptest.NewRequest(http.MethodGet, "/tasks", nil)
+		w := httptest.NewRecorder()
+
+		handler.CreateTask(w, resp)
+
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+		mockRepo.AssertExpectations(t)
+	})
+}
+
 //func TestUserHandlers_Update(t *testing.T) {
 //	// todo testing
 //}
